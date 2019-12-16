84a64d00a23c547104c726ced4f3d42dbfc3ef5d
Joshua Owen <jowen94o@gmail.com>
2019-12-16 11:44:09 +0700

    add user api

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-//hello world
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+const auth = require('./routes/api/auth');
+const app = express();
+
+//Bodyparser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// DB Config
+
+// Connect to mongo
+
+//use Routes
+app.use('/api/auth', auth);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+module.exports = app;
